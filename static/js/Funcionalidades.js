f649a5623d2d701ba9bea773575066e538292705
victorrramirez952 <victory.ramirez@udem.edu>
2023-05-06 00:09:23 -0600

    Ventana modal de productos del menú

--- static/js/Funcionalidades.js
+++ static/js/Funcionalidades.js
@@ -10,6 +10,41 @@ $(document).ready(function () {
               mesa.addClass("bg-gray-300 border-gray-200");
           }
       });
+
+    //   ***************************************
+    $(".btnMasProducto").click(function() {
+        var input = $(this).siblings('input[type="number"]');
+        var value = parseInt(input.val());
+        input.val(value + 1);
+      });
+
+    //   ***************************************
+    $(".btnMenosProducto").click(function() {
+        var input = $(this).siblings('input[type="number"]');
+        var value = parseInt(input.val());
+        if(value != 0) input.val(value - 1);
+      });
+
+      //   ***************************************
+    $(".btnCloseModalProducto").click(function() {
+        $("#modalProducto").hide();
+      });
+
+      //   ***************************************
+      //   ***************************************
+      $(".platillo").click(function(event) {
+        // Check if the clicked element is not a descendant of either excluded div
+      if (!$(event.target).closest('.containerButtons, .btnAgregarCarrito').length) {
+        // Do something if the click was not on an excluded div
+        let platillo = $(this).find(".nombrePlatillo");
+        let descripcion = $(this).find(".descripcionPlatillo");
+        let precioPlatillo = $(this).find(".precioPlatillo");
+        $("#modalProducto .modalNombrePlatillo").text(platillo.text());
+        $("#modalProducto .modalDescripcionPlatillo").text(descripcion.text());
+        $("#modalProducto .modalPrecioPlatillo").text(precioPlatillo.text());
+        $("#modalProducto").css("display", "flex");
+      }
+      });
   
   });
   
